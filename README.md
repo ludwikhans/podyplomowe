27a12cd121aee5d661181fd1fdb70ee30146c03e
gtruty <grzegorztruty@gmail.com>
2021-11-27 14:44:25 +0100

    Dodanie zdjęcia kota

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # podyplomowe
 Program pokazujący jaką mam pracę
+![](https://th.bing.com/th/id/R.8b5c8deab917d9c04a60e10ce352979c?rik=40nnbWV%2bN0cGaQ&pid=ImgRaw&r=0)
--- src/Job.java
+++ src/Job.java
@@ -2,5 +2,6 @@ public class Job {
     public static void main(String[] args) {
         String job = "Nauczyciel";
         System.out.println("Moim zawodem jest: " + job);
+        // Bardzo lubię tą pracę
     }
 }
